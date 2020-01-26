a09cd8848ed9c7500a9333d154b1767b2c74b409
rajeshradhakrishnanmvk <rajeshmvk@gmail.com>
2020-01-26 21:39:30 +0530

    Corrected folder name of Test

--- moments.api/moments.space/docker-run-unittests.sh
+++ moments.api/moments.space/docker-run-unittests.sh
@@ -1,4 +1,4 @@
 `set -eu -o pipefail`
-dotnet restore /code/moments-space.tests/moments-space.tests.csproj
-dotnet build /code/moments-space.tests/moments-space.tests.csproj
-dotnet test  /code/moments-space.tests/
+dotnet restore /code/moments.space.tests/moments.space.tests.csproj
+dotnet build /code/moments.space.tests/moments.space.tests.csproj
+dotnet test  /code/moments.space.tests/
