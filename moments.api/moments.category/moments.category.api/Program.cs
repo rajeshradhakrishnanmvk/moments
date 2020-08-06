9091bd9bedfe107449f2489ca418bfdddf488ee3
rajeshradhakrishnanmvk <rajeshmvk@gmail.com>
2020-08-06 11:09:30 +0530

    [MOM-4] Sentry Setup & Sonar Issue fix

--- moments.api/moments.category/moments.category.api/Program.cs
+++ moments.api/moments.category/moments.category.api/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Sentry;
 
 namespace moments.category.api
 {
@@ -13,7 +14,10 @@ namespace moments.category.api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            using (SentrySdk.Init("https://512517e43b7b4a94bf998f3d472e3334@o405078.ingest.sentry.io/5270124"))
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
